b33d7a111408c6d30743889368ee9c3add30ac23
Seth Yates <95386742+SethY8s@users.noreply.github.com>
2023-11-04 21:10:39 +0700

    working deployment of smart contract

--- smart-contract/hardhat.config.ts
+++ smart-contract/hardhat.config.ts
@@ -3,17 +3,18 @@ import "@nomicfoundation/hardhat-toolbox";
 import "@typechain/hardhat";
 import "@nomicfoundation/hardhat-ethers";
 import "@nomicfoundation/hardhat-chai-matchers";
+import 'dotenv/config';
 
 const key = process.env.KEY as string;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
-  // networks: {
-  //   subspace: {
-  //     url: "https://domain-3.evm.gemini-3g.subspace.network/ws",
-  //     accounts: [key],
-  //   },
-  // },
+  networks: {
+    subspace: {
+      url: "https://domain-3.evm.gemini-3f.subspace.network/ws",
+      accounts: [key],
+    },
+  },
 };
 
 export default config;
