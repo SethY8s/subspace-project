0f95353cad3b5b94b217f187e2834d05910ed534
Seth Yates <95386742+SethY8s@users.noreply.github.com>
2023-11-04 20:50:25 +0700

    final changes to config for deployment

--- smart-contract/hardhat.config.ts
+++ smart-contract/hardhat.config.ts
@@ -1,11 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import '@typechain/hardhat'
-import '@nomicfoundation/hardhat-ethers'
-import '@nomicfoundation/hardhat-chai-matchers'
+import "@typechain/hardhat";
+import "@nomicfoundation/hardhat-ethers";
+import "@nomicfoundation/hardhat-chai-matchers";
+
+const key = process.env.KEY as string;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
+  // networks: {
+  //   subspace: {
+  //     url: "https://domain-3.evm.gemini-3g.subspace.network/ws",
+  //     accounts: [key],
+  //   },
+  // },
 };
 
 export default config;
